7499a69a7fe3e057d7594706253706a92122f627
NobodyIsPerfect <viacheslav.ivanskiy@gmail.com>
2022-03-19 15:03:58 +0300

    middleware complite

--- resources/js/router/router.js
+++ resources/js/router/router.js
@@ -1,11 +1,12 @@
 import {createRouter, createWebHistory} from "vue-router";
-import Container from "./views/layout/Container";
-import Register from "./views/Auth/Register";
-import Login from "./views/Auth/Login";
-import Dashboard from "./views/pages/Dashboard";
-import UserProfile from "./views/pages/UserProfile";
-import Middleware from "./middleware";
-import store from "./store";
+import Container from "../views/layout/Container";
+import Register from "../views/Auth/Register";
+import Login from "../views/Auth/Login";
+import Dashboard from "../views/pages/Dashboard";
+import UserProfile from "../views/pages/UserProfile";
+import Middleware from "../middleware";
+import store from "../store";
+import middlewarePipeline from './middlewarePipeline'
 
 const routes = [
     {
@@ -60,14 +61,15 @@ router.beforeEach((to, from, next) => {
         return next()
     }
     const middleware = to.meta.middleware
-    const context={
+    const context = {
         to,
         from,
         next,
         store,
     }
     return middleware[0]({
-        ...context
+        ...context,
+        next: middlewarePipeline(context, middleware, 1)
     })
 })
 
